254cdd79adcd2654b7ba3b7b1cfd700e75127c82
JuanRodBat <mbjuan_12@hotmail.com>
2025-08-19 17:06:50 -0600

    Update to Netlify

--- unigis-client/src/api/http.js
+++ unigis-client/src/api/http.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+
+const baseURL = process.env.REACT_APP_API_BASE || "https://localhost:5001/api";
+
 export const http = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE,
+  baseURL,
+  timeout: 15000,
 });
